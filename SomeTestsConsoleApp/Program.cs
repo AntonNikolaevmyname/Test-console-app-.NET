17ac09953914e0e5a843d8a0b38635caf02de905
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-01-03 03:36:59 +0300

    just commit

--- SomeTestsConsoleApp/Program.cs
+++ SomeTestsConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SomeTestsConsoleApp
 {
@@ -6,7 +8,24 @@ namespace SomeTestsConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+            while (true)
+            {
+                Console.WriteLine("Введите адрес электронной почты");
+                string email = Console.ReadLine();
+
+                if (Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Email подтвержден");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный email");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
