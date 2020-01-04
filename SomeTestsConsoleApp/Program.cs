a6a00bbb8f19bcdb0d386acbcf8b423fbc8f7597
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-01-04 06:04:38 +0300

    DLR test

--- SomeTestsConsoleApp/Program.cs
+++ SomeTestsConsoleApp/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.IO;
+using System.Text.Json;
 
 namespace SomeTestsConsoleApp
 {
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string dirName = "C:\\Program Files";
+            dynamic x = 3; // здесь x - целочисленное int
+            Console.WriteLine(x);
 
-            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+            x = "Привет мир"; // x - строка
+            Console.WriteLine(x);
 
-            Console.WriteLine($"Название каталога: {dirInfo.Name}");
-            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
-            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+            x = new Person() { Name = "Tom", Age = 23 }; // x - объект Person
+            Console.WriteLine(x);
+
+            Console.ReadLine();
 
             Console.ReadKey();
         }
