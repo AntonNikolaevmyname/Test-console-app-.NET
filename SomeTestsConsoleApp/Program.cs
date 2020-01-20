34cf4a28d7b404249ab477eae02781afb77c2118
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-01-20 23:01:03 +0300

    Static experience

--- SomeTestsConsoleApp/Program.cs
+++ SomeTestsConsoleApp/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Text.Json;
-using print = System.Console;
 
 namespace SomeTestsConsoleApp
 {
-    class Program
+    sealed class Program
     {
-        static dynamic a = new Int32();
+        static Program()
+        {
+            Console.WriteLine("Static ctor");
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
         
-        static object c = new Int32();
         static void Main(string[] args)
         {
-            MethodInfo type = c.GetType().GetMethod("GetType");
-            a = "aaa";
-            //var b = new Int32();
-            print.WriteLine(type.ToString());
             Console.ReadKey();
         }
     }
